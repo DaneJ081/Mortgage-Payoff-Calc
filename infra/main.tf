5402093fed3b6258aa2a5ffd40aed20f3eb6d61d
DaneJ081 <dazer@mitajo.net>
2026-01-07 19:18:44 -0500

    Added precommit check and integrated checks into CI pipeline

--- infra/main.tf
+++ infra/main.tf
@@ -8,7 +8,7 @@ terraform {
     }
   }
 
-    backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "mortcaltfstate"
     container_name       = "tfstate"
